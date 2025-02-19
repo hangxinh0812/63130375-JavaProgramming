6f9602b8d7b885f6988f1142105c8dc2ddddd29e
lê thị thanh hằng <127948428+hangxinh0812@users.noreply.github.com>
2025-02-19 12:00:16 +0700

    hdt
    hdt

---
+++ ViDuHuongDoiTuong/src/SanPham.java
@@ -0,0 +1,21 @@
+
+public class SanPham {
+	// Định nghĩa các thuộc tính
+	int maSP;
+	String tenSP;
+	String loaiSP;
+	String anhSP;   //tên file ảnh
+	// Hàm tạo
+	public SanPham() {
+	    // Khởi tạo giá trị ban đầu cho các thuộc tính nếu có ở đây
+	}
+	public SanPham(int _maSP, String _tenSP, String _loaiSP, String _anhSP) {
+		// Khởi tạo giá trị ban đầu cho các thuộc tính nếu có ở đây
+	    maSP =_maSP;
+	    tenSP = 
+	
+		
+	}
+	
+
+}
