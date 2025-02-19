d082ca96b652d58d3a620b243d1631563d87feba
lê thị thanh hằng <127948428+hangxinh0812@users.noreply.github.com>
2025-02-19 14:11:58 +0700

    Create CT.java

---
+++ ViDuHuongDoiTuong/src/CT.java
@@ -0,0 +1,28 @@
+
+public class CT {
+	
+    public static void main(String[] args) {
+    	
+        // Khai báo danh sách sản phẩm
+        ArrayList<SanPham> dsSanPham;
+
+        // Khởi tạo danh sách
+        dsSanPham = new ArrayList<SanPham>();
+
+        // Nhập 3 sản phẩm
+        SanPham sp1 = new SanPham(0, "Bánh", "Giải khát", null);
+        SanPham sp2 = new SanPham(1, "Kẹo", "Đồ ăn", null);
+        SanPham sp3 = new SanPham(2, "coca", "Giải khát", null);
+
+        // Thêm vào ArrayList
+        dsSanPham.add(sp1);
+        dsSanPham.add(sp2);
+        dsSanPham.add(sp3);
+
+        // Xuất danh sách sản phẩm
+        for (int i = 0; i < dsSanPham.size(); i++) {
+            SanPham sp = dsSanPham.get(i);
+            System.out.println(sp.toString());
+        }
+    }
+}
